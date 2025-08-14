0f9a5b4bfab3d5c7cf90accab9a0b373a00c630a
Ahmad Fauzy <ahmad.fauzy@rollingglory.com>
2025-08-14 17:46:03 +0700

    inisiasi project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
-import Notes from './pages/Notes';
-// import Developer from './pages/Developer/Developer';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import CheckableTag from 'antd/es/tag/CheckableTag';
 import ChecklistBoard from './pages/ChecklistBoard/CheckListBoard';
 
 function App() {
@@ -18,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/checklistboard" element={<ChecklistBoard />} />
-          {/* <Route path="/developer" element={<Developer />} /> */}
         </Routes>
       </Router>
       
